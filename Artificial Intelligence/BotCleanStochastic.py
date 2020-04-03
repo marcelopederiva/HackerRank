89eaeebfaeced03d65979fe62e39a8760949284d
Marcelo Pederiva <46782276+marcelopederiva@users.noreply.github.com>
2020-04-03 12:05:40 -0300

    Add files via upload

---
+++ Artificial Intelligence/BotCleanStochastic.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+def nextMove(posr, posc, board):
+    print ""
+
+if __name__ == "__main__":
+    pos = [int(i) for i in input().strip().split()]
+    board = [[j for j in input().strip()] for i in range(5)]
+    nextMove(pos[0], pos[1], board)
